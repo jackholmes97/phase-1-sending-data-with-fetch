58208bedc96687225c3e91701e64e888232ba383
Jack Holmes <jdh97@comcast.net>
2022-10-17 14:45:37 -0400

    commit tree 8b077750e426d2cad4ca4f939f8951fffbb594f1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userData = {
+        name: userName,
+        email: userEmail,
+    }
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    })
+        .then( (response) => {
+            return response.json(); 
+        })
+        .then( function(object) {
+            document.body.append(object["id"])
+        })
+        .catch( function(error) {
+            document.body.append(error.message);
+        })
+}
+
+submitData("jdh97", "jdh97@comcast.net");
